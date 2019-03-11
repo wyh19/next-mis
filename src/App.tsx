46df218a5bb558d7d792103b58f0a6c66722c083
王焱辉 <wyh_19@163.com>
2019-03-11 11:32:47 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+import * as stores from './store';
+import { hot } from 'react-hot-loader'
+import Home from './page/home/Home'
+import Login from './page/login/Login'
 
 
 class App extends React.Component {
-	public render() {
-		return (
-			<div className="App">
-
-			</div>
-		);
-	}
+    public render() {
+        return (
+            <Provider store={stores}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path='/login' component={Login} />
+                        <Route path='/home' component={Home} />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
+        );
+    }
 }
 
-export default App;
+export default hot(module)(App);
