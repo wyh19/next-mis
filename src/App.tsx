6d451cbe55ead4b9d8e839db0011694b0aed8510
王焱辉 <wyh_19@163.com>
2019-03-11 10:32:55 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-      
-      </div>
-    );
-  }
+	public render() {
+		return (
+			<div className="App">
+
+			</div>
+		);
+	}
 }
 
 export default App;
