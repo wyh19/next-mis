40805b5c599aca5776cf7ab902d7db2ee668b5a5
王焱辉 <wyh_19@163.com>
2019-03-11 14:00:56 +0800

    开发

---
+++ src/page/home/LeftMenuBar.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+
+class LeftMenuBar extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default LeftMenuBar
